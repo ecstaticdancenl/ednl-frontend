478d2120beff51bf397740cd01ec34c456d05603
How About Yes <sefrijn@gmail.com>
2023-04-20 21:35:41 +0200

    Improve preview mode

--- pages/locaties/preview.tsx
+++ pages/locaties/preview.tsx
@@ -8,6 +8,8 @@ import Lottie from "react-lottie";
 import loading from "@/public/dancing.json";
 import Locatie from "@/pages/locaties/[locatie]";
 import { useRouter } from "next/router";
+import basepath from "@/lib/basepath";
+import WordpressLogo from "@/components/wordpressLogo";
 
 async function getDynamicProps(slug: string) {
   //    Get data from WordPress
@@ -98,11 +100,26 @@ export default function Preview() {
 
   if (data) {
     return (
-      <Locatie
-        organisation={data.organisation}
-        addresses={data.addresses}
-        defaultImages={data.defaultImages}
-      />
+      <>
+        <div
+          onClick={() => {
+            history.back();
+          }}
+          className={
+            "bg-gray-900 fixed top-0 z-[99] w-full text-xs py-1 px-2.5 cursor-pointer hover:bg-gray-800 hover:text-blue-300 text-white flex items-center gap-2 group"
+          }
+        >
+          <WordpressLogo
+            className={"w-5 h-5 text-gray-400 group-hover:text-blue-300"}
+          />
+          <span>Preview modus. Klik hier om terug te gaan naar Wordpress.</span>
+        </div>
+        <Locatie
+          organisation={data.organisation}
+          addresses={data.addresses}
+          defaultImages={data.defaultImages}
+        />
+      </>
     );
   }
   return (
