79510c772ae338f14b73935d6fc7bada3cfa3775
How About Yes <sefrijn@gmail.com>
2023-05-01 23:52:38 +0200

    Bugfix locatie width

--- pages/locaties/[locatie].tsx
+++ pages/locaties/[locatie].tsx
@@ -103,7 +103,7 @@ export default function Locatie({ organisation }: AppProps) {
       <OrgHeader organisation={organisation} />
       <div
         className={
-          "grid grid-cols-1 lg:grid-cols-3 lg:gap-8 my-10 gap-y-8 lg:px-10 sm:px-6 px-4"
+          "w-full grid grid-cols-1 lg:grid-cols-3 lg:gap-8 my-10 gap-y-8 lg:px-10 sm:px-6 px-4"
         }
       >
         <div className={"lg:sticky top-12 self-start lg:mb-6"}>
