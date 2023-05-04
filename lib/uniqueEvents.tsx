1b559b96d6b1d90808af8c0218868835005bbd4e
How About Yes <sefrijn@gmail.com>
2023-05-04 15:20:52 +0200

    Bugfix unique events

--- lib/uniqueEvents.tsx
+++ lib/uniqueEvents.tsx
@@ -3,11 +3,8 @@ import { parseISO } from "date-fns";
 
 export function uniqueEvents(events: any) {
   const uniqueTitles: { [key: string]: boolean } = {};
-  const uniqueDates: { [key: string]: boolean } = {};
   const uniqueURL: { [key: string]: boolean } = {};
-  return events.filter((event: any) => {
-    const eventURL = event.url_hipsy ? event.url_hipsy : event.ticket_uri;
-    const eventTitle = event.name ? event.name : event.title;
+  const filteredEvents = events.filter((event: any) => {
     let eventTime: Date | null = null;
     if (event.type == "Hipsy") {
       eventTime = utcToZonedTime(
@@ -20,18 +17,20 @@ export function uniqueEvents(events: any) {
     }
 
     if (eventTime) {
-      if (
-        (uniqueTitles[eventTitle] && uniqueDates[eventTime.toDateString()]) ||
-        (uniqueDates[eventTime.toDateString()] && uniqueURL[eventURL])
-      ) {
+      const eventURL =
+        eventTime.toDateString() +
+        (event.url_hipsy ? event.url_hipsy : event.ticket_uri);
+      const eventTitle =
+        eventTime.toDateString() + (event.name ? event.name : event.title);
+      if (uniqueTitles[eventTitle] || uniqueURL[eventURL]) {
         return false;
       }
       uniqueTitles[eventTitle] = true;
-      uniqueDates[eventTime.toDateString()] = true;
       uniqueURL[eventURL] = true;
       return true;
     } else {
       return false;
     }
   });
+  return filteredEvents;
 }
