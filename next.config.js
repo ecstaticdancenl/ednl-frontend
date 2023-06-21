4a001baa0b71564f5c4deff012e082e71d45975e
Ecstatic Dance Nederland <131710239+ecstaticdancenl@users.noreply.github.com>
2023-06-21 14:04:27 +0200

    Update next.config.js
    to fix errors fetching hipsy event data (around 130kb) default was 128kb cut-off.
    RRR

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
+  experimental: {
+    largePageDataBytes: 256 * 1000,
+  }, 
+
 };
 
 module.exports = nextConfig;
