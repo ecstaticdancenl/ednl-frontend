35bbf7b2782dc775fd8b4e1838105dd791c43ff7
Ecstatic Dance Nederland <131710239+ecstaticdancenl@users.noreply.github.com>
2023-10-26 21:16:02 +0200

    Update getEventsFromFacebook.tsx
    debug if any fb events coming through, limit to 5 -> log

--- lib/getEventsFromFacebook.tsx
+++ lib/getEventsFromFacebook.tsx
@@ -1,8 +1,10 @@
 export async function getEventsFromFacebook() {
   const result = await fetch(
-    `https://graph.facebook.com/v13.0/EDNederland/events?access_token=${process.env.FACEBOOK}&limit=50&time_filter=upcoming&fields=name,start_time,place,cover.source(width(162),height(126)),ticket_uri`
+    `https://graph.facebook.com/v13.0/EDNederland/events?access_token=${process.env.FACEBOOK}&limit=5&time_filter=upcoming&fields=name,start_time,place,cover.source(width(162),height(126)),ticket_uri`
   );
-
+ console.log(
+        "Getting FB events" + result.json()
+  );
   const dataFacebook = await result.json();
   const eventsFacebookRaw = dataFacebook.data.map((event: any) => {
     event.type = "Facebook";
