e359a8a80a42eeeea205913c66345089b507ea7f
Ecstatic Dance Nederland <131710239+ecstaticdancenl@users.noreply.github.com>
2023-10-26 22:11:50 +0200

    Update getEventsFromFacebook.tsx
    giving up...

--- lib/getEventsFromFacebook.tsx
+++ lib/getEventsFromFacebook.tsx
@@ -1,14 +1,11 @@
 export async function getEventsFromFacebook() {
   const result = await fetch(
-    `https://graph.facebook.com/v18.0/EDNederland/events?access_token=${process.env.FACEBOOK}&limit=5&time_filter=upcoming&fields=name,start_time,place,cover.source(width(162),height(126)),ticket_uri`
+    `https://graph.facebook.com/v13.0/EDNederland/events?access_token=${process.env.FACEBOOK}&limit=5&time_filter=upcoming&fields=name,start_time,place,cover.source(width(162),height(126)),ticket_uri`
   );
  console.log(
         "Getting FB events"
   );
   const dataFacebook = await result.json();
-
-  console.log("name:" + dataFacebook.name);
-
   const eventsFacebookRaw = dataFacebook.data.map((event: any) => {
     event.type = "Facebook";
     return event;
